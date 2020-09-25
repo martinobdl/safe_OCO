8c468c0092a2854bac27212f2c0e0d14c8cce562
martinobdl <martino.n.bernasconi@gmail.com>
2020-09-25 17:42:51 +0200

    modified utils

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,36 @@ def project(v):
     return np.maximum(v-theta, 0)
 
 
+def clean_dump_vector(a: str) -> np.ndarray:
+
+    def _clean(x: str) -> float:
+        if x == '':
+            return None
+        elif ' ' in x:
+            x = x.replace(' ', '')
+            return _clean(x)
+        elif '\n' in x:
+            x = x.replace('\n', '')
+            return _clean(x)
+        elif '[' in x:
+            x = x.replace('[', '')
+            return _clean(x)
+        elif ']' in x:
+            x = x.replace(']', '')
+            return _clean(x)
+        elif x.replace('.', '', 1).isdigit():
+            return float(x)
+        else:
+            breakpoint()
+            raise Exception("{} is an invalid string".format(x))
+
+    sol = []
+    for x in a.split(' '):
+        clean_x = _clean(x)
+        if clean_x is not None:
+            sol.append(_clean(x))
+    return np.array(sol)
+
+
 if __name__ == "__main__":
-    v = np.random.randn(10)
-    assert np.abs(np.sum(project(v)) - 1) < 1e-3, f"projected in {np.sum(project(v))}"
+    pass
