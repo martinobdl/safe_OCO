b3ce48a885ccef3d53e87f7a4fdd31e8d6741c37
martinobdl <martino.n.bernasconi@gmail.com>
2020-09-25 17:42:33 +0200

    added no projection option

--- src/OGD.py
+++ src/OGD.py
@@ -4,15 +4,20 @@ import utils
 
 class OGD(Strategy):
 
-    def __init__(self, x0, beta_0=0.5):
+    def __init__(self, x0, beta_0=0.5, projection='simplex'):
         super().__init__()
         self.beta_0 = beta_0
         self.x0 = x0
+        self.projection = projection
 
     def _forward(self, feedback):
         self.t += 1
         grad_t = feedback["grad_t"]
-        x_t1 = utils.project(self.x_t - self.beta_0*grad_t/self.t**0.5)
+        y_t1 = self.x_t - self.beta_0*grad_t/self.t**0.5
+        if self.projection == 'simplex':
+            x_t1 = utils.project(y_t1)
+        else:
+            x_t1 = y_t1
         self.x_t = x_t1
         prediction = {}
         prediction["x_t"] = x_t1
@@ -31,18 +36,4 @@ class OGD(Strategy):
 
 
 if __name__ == "__main__":
-
-    import numpy as np
-    from linear_regression import OLR
-    from experiment import Experiment
-
-    n = 2
-    x0 = np.ones(n)/n
-    algo = OGD(x0)
-    env = OLR(2, max_T=1000)
-    exp = Experiment(algo, env)
-    exp.seed(1)
-    exp.run()
-
-    print("True: ", env.beta)
-    print("OGD: ", algo.x_t)
+    pass
