d437819585152a82b42b0aa819c7081d636624a4
martinobdl <martino.n.bernasconi@gmail.com>
2020-10-01 08:56:01 +0200

    Update IMDB_main.py

--- src/IMDB_main.py
+++ src/IMDB_main.py
@@ -1,6 +1,7 @@
 from OGD import OGD
 from COGD import COGD
 from DPOGD import DPOGD
+from DPOGDMAX import DPOGDMAX
 from ADAM import ADAM
 from experiment import Experiment
 from IMDB_env import SafeIMDB
@@ -32,29 +33,36 @@ if __name__ == "__main__":
     base = OGD(x0, K_0, projection=None)
     algo_dp = DPOGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
     algo_c = COGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
-    adam = ADAM(x0=x0)
+    adam = ADAM(x0=x0, alpha=1)
+    dpogdmax = DPOGDMAX(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
 
-    print("running DPOGD")
-    exp = Experiment(algo_dp, env, check_point=check_point)
-    exp.run()
-    exp.save(folder=folder)
+    # print("running DPOGD")
+    # exp = Experiment(algo_dp, env, check_point=check_point)
+    # exp.run()
+    # exp.save(folder=folder)
 
-    print("running OGD")
-    exp2 = Experiment(base, env, check_point=check_point)
-    exp2.run()
-    exp2.save(folder=folder)
+    # print("running OGD")
+    # exp2 = Experiment(base, env, check_point=check_point)
+    # exp2.run()
+    # exp2.save(folder=folder)
 
-    print("running COGD")
-    exp3 = Experiment(algo_c, env, check_point=check_point)
-    exp3.run()
-    exp3.save(folder=folder)
+    # print("running COGD")
+    # exp3 = Experiment(algo_c, env, check_point=check_point)
+    # exp3.run()
+    # exp3.save(folder=folder)
 
     print("running ADAM")
     exp4 = Experiment(adam, env, check_point=check_point)
     exp4.run()
     exp4.save(folder=folder)
 
+    print("running ADAM")
+    exp5 = Experiment(dpogdmax, env, check_point=check_point)
+    exp5.run()
+    exp5.save(folder=folder)
+
     print("accuracy DPOGD: ", utils.accuracy(env, algo_dp.x_t))
+    print("accuracy DPOGDMAX: ", utils.accuracy(env, dpogdmax.x_t))
     print("accuracy COGD: ", utils.accuracy(env, algo_c.x_t))
     print("accuracy DPOGD base: ", utils.accuracy(env, algo_dp.base.x_t))
     print("accuracy COGD base: ", utils.accuracy(env, algo_c.base.x_t))
