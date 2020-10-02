363c3d876c2284bbdd5470a1ee16ee64d589c204
martinobdl <martino.n.bernasconi@gmail.com>
2020-10-02 22:00:47 +0200

    add SPAM env

---
+++ src/SPAM_env.py
@@ -0,0 +1,127 @@
+from env import Env
+import numpy as np
+import utils
+
+
+class SPAM(Env):
+    def __init__(self, times=1, rnd=1):
+        self.rnd = rnd
+        self.X = np.load('./data/spam.npy')
+        self.target = np.load('./data/spam_target.npy')
+        self.lam = 0
+        self.beta_best = np.load('./data/beta_logistic_spam_best.npy')[0]
+        self.max_T = self.X.shape[0]*times
+
+    def step(self, prediction):
+        idx = np.random.randint(0, len(self.X))
+        x = self.X[idx, :]
+        y = self.target[idx]
+        H = prediction["x_t"]
+
+        y_hat = utils.logit(np.dot(H, x))
+        y_best = utils.logit(np.dot(self.beta_best, x))
+
+        def loss_fnc(p):
+            return -y*np.log(p) - (1-y)*np.log(1-p)
+
+        loss_t = np.array([loss_fnc(y_hat) + self.lam/2 * np.linalg.norm(H)**2 / self.max_T])
+        grad_t = x*(-y*(1-y_hat)+(1-y)*y_hat)
+
+        feedback = {
+                    "best_loss_t": np.array([loss_fnc(y_best)+self.lam/2*np.linalg.norm(self.beta_best)**2/self.max_T]),
+                    "loss_t": loss_t,
+                    "grad_t": grad_t
+                    }
+        self.t += 1
+
+        return feedback
+
+    def restart(self):
+        self.t = 0
+        self.seed()
+
+    def done(self):
+        return self.t >= self.max_T
+
+    def to_dict(self):
+        return {
+                "name": "SPAMLogistic",
+                "beta_best": str(self.beta_best),
+                "T": self.max_T,
+                "lambda": self.lam,
+                "rnd": self.rnd
+                }
+
+
+class SafeSPAM(SPAM):
+    def __init__(self, times=1, rnd=1):
+        super().__init__(times, rnd)
+        self.beta_def = np.load('./data/beta_logistic_spam_weak.npy')[0]
+
+    def step(self, prediction):
+        idx = np.random.randint(0, len(self.X))
+        x = self.X[idx, :]
+        y = self.target[idx]
+        H = prediction["x_t"]
+        H_LR = prediction["x_LR"]
+
+        y_hat = utils.logit(np.dot(H, x))
+        y_best = utils.logit(np.dot(self.beta_best, x))
+        y_def = utils.logit(np.dot(self.beta_def, x))
+        y_LR = utils.logit(np.dot(H_LR, x))
+
+        def loss_fnc(p):
+            return -y*np.log(p) - (1-y)*np.log(1-p)
+
+        def grad_fnc(p):
+            return x*(-y*(1-p)+(1-y)*p)
+
+        loss_t = np.array([loss_fnc(y_hat) + self.lam/2 * np.linalg.norm(H)**2 / self.max_T])
+        grad_t = grad_fnc(y_LR)
+        best_loss_t = np.array([loss_fnc(y_best)+self.lam/2*np.linalg.norm(self.beta_best)**2/self.max_T])
+        loss_def_t = np.array([loss_fnc(y_def)+self.lam/2*np.linalg.norm(self.beta_def)**2/self.max_T])
+
+        feedback = {
+                    "recc_t": self.beta_def,
+                    "loss_def_t": loss_def_t,
+                    "best_loss_t": best_loss_t,
+                    "loss_t": loss_t,
+                    "grad_t": grad_t
+                    }
+        self.t += 1
+
+        return feedback
+
+    def to_dict(self):
+        return {
+                "name": "SPAMLogisticSafe",
+                "beta_best": str(self.beta_best),
+                "beta_def": str(self.beta_def),
+                "T": self.max_T,
+                "lambda": self.lam
+                }
+
+
+if __name__ == "__main__":
+    from ADAGRAD import ADAGRAD
+    from experiment import Experiment
+
+    n = 57
+    x0 = np.ones(n)/n
+
+    alpha = 0.01
+    e_l = 0
+    nk = n
+    c = 2
+    e_u = nk*c
+    G = nk**0.5
+    D = (n*c*2)**0.5
+    K_0 = D/G/2**0.5
+
+    env = SafeSPAM(times=5)
+    algo = ADAGRAD(x0)
+    exp = Experiment(algo, env)
+    exp.run()
+
+    print("accuracy algo: ", utils.accuracy(env, algo.x_t))
+    print("accuracy best :", utils.accuracy(env, env.beta_best))
---
+++ src/SPAM_main.py
@@ -0,0 +1,76 @@
+from OGD import OGD
+from COGD import COGD
+from DPOGD import DPOGD
+from DPOGDMAX import DPOGDMAX
+from ADAM import ADAM
+from ADAGRAD import ADAGRAD
+from experiment import Experiment
+from SPAM_env import SafeSPAM
+import utils
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    n = 57
+    x0 = np.ones(n)/n
+
+    alpha = 0.01
+    e_l = 1e-4
+    nk = n
+    c = 2
+    e_u = 10
+    G = nk**0.5
+    D = (n*c*2)**0.5
+    K_0 = D/G/2**0.5
+
+    rnd = 1
+
+    env = SafeSPAM(times=100, rnd=rnd)
+
+    check_point = 1
+    folder = "experiments/SPAM"
+
+    base = OGD(x0, K_0, projection=None)
+    algo_dp = DPOGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
+    algo_c = COGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
+    adam = ADAM(x0=x0, alpha=1)
+    adagrad = ADAGRAD(x0=x0)
+    dpogdmax = DPOGDMAX(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u, projection=None)
+
+    print("running DPOGD")
+    exp = Experiment(algo_dp, env, check_point=check_point)
+    exp.run()
+    exp.save(folder=folder)
+
+    print("running OGD")
+    exp2 = Experiment(base, env, check_point=check_point)
+    exp2.run()
+    exp2.save(folder=folder)
+
+    print("running COGD")
+    exp3 = Experiment(algo_c, env, check_point=check_point)
+    exp3.run()
+    exp3.save(folder=folder)
+
+    # print("running ADAM")
+    # exp4 = Experiment(adam, env, check_point=check_point)
+    # exp4.run()
+    # exp4.save(folder=folder)
+
+    # print("running DPOGDMAX")
+    # exp5 = Experiment(dpogdmax, env, check_point=check_point)
+    # exp5.run()
+    # exp5.save(folder=folder)
+
+    print("running ADAGRAD")
+    exp6 = Experiment(adagrad, env, check_point=check_point)
+    exp6.run()
+    exp6.save(folder=folder)
+
+    print("accuracy DPOGD: ", utils.accuracy(env, algo_dp.x_t))
+    print("accuracy COGD: ", utils.accuracy(env, algo_c.x_t))
+    print("accuracy OGD: ", utils.accuracy(env, base.x_t))
+    print("accuracy ADAGRAD: ", utils.accuracy(env, adagrad.x_t))
+    print("accuracy def: ", utils.accuracy(env, env.beta_def))
+    print("accuracy best :", utils.accuracy(env, env.beta_best))
