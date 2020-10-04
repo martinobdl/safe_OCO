8c48bda6fe2f6fb5aedab31c0d39e840059895df
martinobdl <martino.n.bernasconi@gmail.com>
2020-10-04 13:00:32 +0200

    Update SPAM_env.py

--- src/SPAM_env.py
+++ src/SPAM_env.py
@@ -69,7 +69,10 @@ class SafeSPAM(SPAM):
         x = self.X[idx, :]
         y = self.target[idx]
         H = prediction["x_t"]
-        H_LR = prediction["x_LR"]
+        if "x_LR" in prediction.keys():
+            H_LR = prediction["x_LR"]
+        else:
+            H_LR = prediction["x_t"]
 
         y_hat = utils.logit(np.dot(H, x))
         y_best = utils.logit(np.dot(self.beta_best, x))
