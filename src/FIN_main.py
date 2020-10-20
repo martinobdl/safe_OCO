c838a8f580c439277c0689600fa678e8f6b0ae3f
martinobdl <martino.n.bernasconi@gmail.com>
2020-10-20 15:58:49 +0200

    final2

--- src/FIN_main.py
+++ src/FIN_main.py
@@ -39,15 +39,9 @@ if __name__ == "__main__":
     dpogd = DPOGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u)
     dpogdmax = DPOGDMAX(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u)
     cogd = COGD(x0=x0, K_0=K_0, alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u)
-    adagrad = ADAGRAD(x0=x0)
-    assets = utils.clean_dump_vector("""[195 251 245 296 85 2 485 106 86 157 319 54 136 336 499 173
-                                         408 401 440 388 473 439 306 125 327 54 192 3 383 411 65 139
-                                         17 313 447 48 299 324 131 160 351 413 89 217 339 474 188
-                                         80 456 179 436 395 273 393 2 235 363 402 38 264 51 277 257
-                                         30 372 223 149 322 501 130 290 190 345 434 56 371 372 347 374 185
-                                         23 281 468 123 389 267 415 338 176 157 214 196  20 299 384 348 196
-                                         237 236 264]""").astype('int')
 
+    np.random.seed(1079)
+    assets = np.random.choice(467, size=n)
     env = GBM_safe(base_strategy=bs, assets_idxs=assets)
 
     exp = Experiment(dpogd, env, check_point=check_point)
