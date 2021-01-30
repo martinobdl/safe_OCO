63f8e69e81170d1c7b0c1573f5a84f9ed8262332
martinobdl <martino.n.bernasconi@gmail.com>
2021-01-30 20:14:38 +0100

    individual algo

--- src/OLR_main2.py
+++ src/OLR_main2.py
@@ -16,8 +16,6 @@ if __name__ == "__main__":
     n = 40
     m = 1
     beta = np.zeros(n)
-    # beta[0] = m
-    # beta[-1] = -m
     baselinex0 = beta.copy()
     alpha = 0.01
     x0 = np.random.uniform(size=n)*2*m-m
@@ -31,18 +29,19 @@ if __name__ == "__main__":
 
     print(G*D-alpha*e_l)
 
-    check_point = 10
+    check_point = 1
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-D', type=float, default=0.2)
+    parser.add_argument('-D', type=float, default=1.5)
     parser.add_argument('-seed', type=int, default=1)
+    parser.add_argument('-algo', type=int, default="dpwrap")
     args = parser.parse_args()
 
     projection = lambda x: utils.project_fixed(x, c)
 
     Dtilde = args.D
     seed = args.seed
-    baselinex0 = np.random.uniform(size=n)*2*m-m
+    baselinex0 = np.arange(n)/n
     conv = 1-Dtilde/np.linalg.norm(baselinex0 - beta)
     baselinex0 = beta*conv + (1-conv)*baselinex0
     print(np.linalg.norm(baselinex0 - beta))
@@ -51,7 +50,6 @@ if __name__ == "__main__":
     baseline = ConstantStrategy(x0=baselinex0)
 
     folder = "experiments2/OLR"
-    folder = "/tmp"
 
     base = OGD(x0, K_0, projection)
     adagrad = ADAGRAD(x0=x0)
@@ -59,50 +57,19 @@ if __name__ == "__main__":
     cwrap = CWRAP(OGD(x0, K_0, projection), alpha=alpha, G=G, D=D, e_l=e_l, e_u=e_u)
     algo0 = RewardDoublingNDGuess(baselinex0, alpha*e_l/2)
     reward2 = ConversionConstraint(algo0, projection)
-    env = SafeOLR(baseline, n, max_T=100000, beta=beta, rnd=seed, eps_m=0.8)
+    env = SafeOLR(baseline, n, max_T=100000, beta=beta, rnd=seed, eps_m=1)
 
-    exp1 = Experiment(dpwrap, env, check_point=check_point)
-    exp1.run()
-    exp1.save(folder=folder)
+    if args.algo == 'dpwrap':
+        algo = dpwrap
+    if args.algo == 'cwrpa':
+        algo = cwrap
+    if args.algo == 'adagrad':
+        algo = adagrad
+    if args.algo == 'ogd':
+        algo = base
+    if args.algo == 'r2':
+        algo = reward2
 
-    exp2 = Experiment(cwrap, env, check_point=check_point)
-    exp2.run()
-    exp2.save(folder=folder)
-
-    exp3 = Experiment(base, env, check_point=check_point)
-    exp3.run()
-    exp3.save(folder=folder)
-
-    exp4 = Experiment(adagrad, env, check_point=check_point)
-    exp4.run()
-    exp4.save(folder=folder)
-
-    exp5 = Experiment(reward2, env, check_point=check_point)
-    exp5.run()
-    exp5.save(folder=folder)
-
-    plt.figure()
-    plt.semilogy(exp1.history['L_t']-exp1.history['LS_t'])
-    plt.semilogy(exp2.history['L_t']-exp2.history['LS_t'])
-    plt.semilogy(exp3.history['L_t']-exp3.history['LS_t'])
-    plt.semilogy(exp4.history['L_t']-exp4.history['LS_t'])
-    plt.semilogy(exp5.history['L_t']-exp5.history['LS_t'])
-    plt.legend(['dp', 'c', 'ogd', 'adagrad', 'r2'])
-    plt.title('RT')
-    plt.show()
-
-    plt.figure()
-    plt.plot(-(exp1.history['L_t']-exp1.history['LT_t']*(1+alpha)))
-    plt.plot(-(exp2.history['L_t']-exp2.history['LT_t']*(1+alpha)))
-    plt.plot(-(exp3.history['L_t']-exp3.history['LT_t']*(1+alpha)))
-    plt.plot(-(exp4.history['L_t']-exp4.history['LT_t']*(1+alpha)))
-    plt.plot(-(exp5.history['L_t']-exp5.history['LT_t']*(1+alpha)))
-    plt.legend(['dp', 'c', 'ogd', 'adagrad', 'r2'])
-    plt.title('ZT')
-    plt.show()
-
-    plt.figure()
-    plt.plot(exp2.history['beta'])
-    plt.plot(exp1.history['beta'])
-    plt.legend(['c', 'dp'])
-    plt.show()
+    exp = Experiment(algo, env, check_point=check_point)
+    exp.run()
+    exp.save(folder=folder)
